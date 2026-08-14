06f1aee2c2249b3a14e0865d6be5607cb7d133b3
Shubhouy1 <shubhrastogi725@gmail.com>
2026-08-14 14:38:07 +0530

    Add Azure Container Apps CLI setup

---
+++ day5/piece3/container-apps.ps1
@@ -0,0 +1,5 @@
+az group create -n thinkschool-rg -l centralindia
+
+az containerapp env create -n thinkschool-env -g thinkschool-rg -l koreacentral
+
+az containerapp env show -n thinkschool-env -g thinkschool-rg -o json
